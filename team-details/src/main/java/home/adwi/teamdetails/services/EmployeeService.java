76578424aa3f41df7ad0e3f933830bc69df57681
Adwit <y4yanamala@gmail.com>
2020-03-15 01:53:56 +0530

    e2e flow - adding missing files from prev commit

---
+++ team-details/src/main/java/home/adwi/teamdetails/services/EmployeeService.java
@@ -0,0 +1,24 @@
+package home.adwi.teamdetails.services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import home.adwi.teamdetails.entity.Employee;
+import home.adwi.teamdetails.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	public List<Employee> getAllEmployees(){
+		// return Arrays.asList(new Employee(1,"Obi","Lead"),new Employee(2,"Pramod","Senior Analyst"));
+		List<Employee> employees = new ArrayList<>();
+		employeeRepository.findAll().forEach(employees::add);
+		return employees;
+	}
+}
